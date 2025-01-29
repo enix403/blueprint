1a77d8ab6b969f4bd62a98c5a59b96701c597e09
enix403 <fog.code000@gmail.com>
2025-01-29 15:58:55 +0500

    better filter for corner detection

--- minimal/walls.py
+++ minimal/walls.py
@@ -7,3 +7,23 @@ make sure extra "joined" bits of walls are properly
     connected to representation
 
 """
+
+def detect_unjoined_corners(grid):
+
+    grid = grid.float().unsqueeze(0).unsqueeze(0)
+
+    fbl = torch.tensor([
+        [0,  0,  2,  0,  0],
+        [0,  0,  2, -1,  0],
+        [0,  0, -1,  2,  2],
+        [0,  0,  0,  0,  0],
+        [0,  0,  0,  0,  0],
+    ], dtype=torch.float32).unsqueeze(0).unsqueeze(0)
+
+
+    res = F.conv2d(grid, fbl, padding=2)
+    res = (res == 8).byte()
+
+    res = res.squeeze()
+
+    return res
