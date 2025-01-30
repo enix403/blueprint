03534758b9b705611ebb0088ebcb4d823453fb9f
enix403 <fog.code000@gmail.com>
2025-01-30 17:31:39 +0500

    orientation masking

--- minimal/wall_corners.py
+++ minimal/wall_corners.py
@@ -1,32 +1,51 @@
 import torch
 
 
-kern1 = torch.tensor([
+# disonnected corners
+_kerns1 = torch.tensor([
     [-1,  2,  0],
     [ 2, -1,  0],
     [ 0,  0,  0]
 ], dtype=torch.int8)
 
 all_kerns1 = [
-    kern1,
-    torch.flip(kern1, (0,)),
-    torch.flip(kern1, (1,)),
-    torch.flip(kern1, (0, 1)),
+    _kerns1,
+    torch.flip(_kerns1, (0,)),
+    torch.flip(_kerns1, (1,)),
+    torch.flip(_kerns1, (0, 1)),
 ]
 
 # =====================
 
-kern2 = torch.tensor([
+# extra corners
+_kerns2 = torch.tensor([
     [ 2,  0,  0],
     [ 2,  2,  0],
     [ 2,  2,  2]
 ], dtype=torch.int8)
 
 all_kerns2 = [
-    kern2,
-    torch.flip(kern2, (0,)),
-    torch.flip(kern2, (1,)),
-    torch.flip(kern2, (0, 1)),
+    _kerns2,
+    torch.flip(_kerns2, (0,)),
+    torch.flip(_kerns2, (1,)),
+    torch.flip(_kerns2, (0, 1)),
 ]
 
 # =====================
+
+# L shaped corner for orientation masking
+
+_kerns3 = torch.tensor([
+    [ 0,  1,  0],
+    [ 1,  0,  0],
+    [ 0,  0,  0]
+], dtype=torch.int8)
+
+all_kerns3 = [
+    _kerns3, # R + B
+    torch.flip(_kerns3, (0,)), # T + R
+    torch.flip(_kerns3, (1,)), # B + L
+    torch.flip(_kerns3, (0, 1)), # T + L
+]
+
+
