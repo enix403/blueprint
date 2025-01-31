ba4791bf869e6233eccbb6b0bafacc7317ca84bc
enix403 <fog.code000@gmail.com>
2025-01-31 20:01:52 +0500

    Refactor

--- minimal/imaging.py
+++ minimal/imaging.py
@@ -23,7 +23,11 @@ def draw_plan(
     return plan_img.resize((img_size, img_size), Image.Resampling.BOX)
 
 
-def blit_rooms(rooms: list, out_size=256):
+def blit_rooms(
+    rooms: list,
+    sep_mask=None,
+    out_size=256
+):
     h = rooms[0].grid_height
     w = rooms[0].grid_width
 
@@ -38,6 +42,11 @@ def blit_rooms(rooms: list, out_size=256):
         r, g, b = webcolors.hex_to_rgb(NODE_COLOR[n])
         draw.bitmap((0, 0), mask_bitmap, fill=(r, g, b))
 
+    if sep_mask is not None:
+        walls = (sep_mask > 0).byte()
+        plan_img = plan_img.resize(tuple(walls.shape), Image.Resampling.BOX)
+
+        mask_bitmap = Image.fromarray(walls.numpy() * 255, mode="L")
+        ImageDraw.Draw(plan_img).bitmap((0, 0), mask_bitmap, fill=(0, 0, 0))
+
     return plan_img.resize((out_size, out_size), Image.Resampling.BOX)
-
-
