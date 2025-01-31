be929681f9db8b6d78da34636470a30dd7ab0bed
enix403 <fog.code000@gmail.com>
2025-01-31 20:33:33 +0500

    Create a vectorized version of create_sep_mask

--- minimal/walls.py
+++ minimal/walls.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 
 CC_TL = 1
 CC_TR = 2
@@ -81,3 +82,68 @@ def scale_sep_mask(
             view[ :,  0] += src & CC_L
 
     return scaled_sep_mask
+
+# -----------------------------------
+
+"""
+len_x, len_y = room_mask.shape
+
+sep_mask = torch.zeros(room_mask.shape, dtype=torch.uint8)
+
+for x in range(len_x):
+    for y in range(len_y):
+        cell_room = room_mask[x, y]
+
+        for dx, dy, bit in _dirs:
+            nx = x + dx
+            ny = y + dy
+
+            if nx < 0 or nx >= len_x or ny < 0 or ny >= len_y:
+                continue
+
+            n_room = room_mask[nx, ny]
+
+            if cell_room > n_room:
+                sep_mask[x, y] += bit
+
+return sep_mask
+"""
+
+def _conv_mask(mask, kernel, threshold_match = None):
+    mask = mask.to(torch.int8).unsqueeze(0).unsqueeze(0)
+    # kernel is assumed to be in int8
+    kernel = kernel.unsqueeze(0).unsqueeze(0)
+
+    f = kernel.shape[-1]
+    padding = (f - 1) // 2
+
+    result = F.conv2d(mask, kernel, padding=padding)
+    result = result[0, 0, :, :]
+
+    if threshold_match is not None:
+        result = (result == threshold_match).byte()
+
+    return result
+
+f = torch.tensor([
+    [CC_TL,  CC_T, CC_TR],
+    [CC_L ,     0,  CC_R],
+    [CC_BL,  CC_B, CC_BR],
+], dtype=torch.int8)
+
+def create_sep_mask_fast(room_mask: torch.tensor):
+    room_mask = room_mask + 1
+    num_rooms = torch.max(room_mask)
+
+    sep_mask = torch.zeros_like(room_mask)
+
+    for r in range(2, num_rooms + 1):
+        potential_boundary = (room_mask < r).byte()
+        global_seps = _conv_mask(potential_boundary, f)
+        global_seps[room_mask != r] = 0
+
+        sep_mask += global_seps
+
+    return sep_mask
+
+
