88e1017132778b42143d50d57359ad7fbe2775dc
enix403 <fog.code000@gmail.com>
2025-02-03 22:39:17 +0500

    collect wall runs

--- minimal/doors.py
+++ minimal/doors.py
@@ -47,61 +47,22 @@ def extract_face_walls(sep_mask):
     return [up_walls, right_walls, down_walls, left_walls]
 
 
-def candidate_walls(
-    face_walls,
-    room_mask,
-    ra, rb
-):
-    if ra < rb:
-        ra, rb = rb, ra
-    for i, fw in enumerate(face_walls):
-        ws = fw.clone()
-        ws[room_mask != ra] = 0
-        ws = restrict_touching(room_mask, ws, ra, rb)
+# def candidate_walls(
+#     face_walls,
+#     room_mask,
+#     ra, rb
+# ):
+#     if ra < rb:
+#         ra, rb = rb, ra
 
-        loc_x, loc_y = torch.where(ws > 0)
+#     for i, fw in enumerate(face_walls):
+#         ws = fw.clone()
+#         ws[room_mask != ra] = 0
+#         ws = restrict_touching(room_mask, ws, ra, rb)
 
-def restrict_touching(room_mask, ra_walls, ra, rb):
-    room_mask = room_mask + 1
-    ra = ra + 1
-    rb = rb + 1
-    
-    room_mask[room_mask == ra] = 0
-    room_mask[room_mask == rb] = 0
-
-    restricted = (_conv_mask(room_mask, _res_kernel) == 0).byte()
-    restricted[ra_walls == 0] = 0
-
-    return restricted
+#         loc_x, loc_y = torch.where(ws > 0)
 
 
-_res_kernel = torch.tensor([
-    [1, 1, 1],
-    [1, 0, 1],
-    [1, 1, 1],
-], dtype=torch.int8)
+"""
 
-
-def extract_walls_runs(lx, ly, min_len: int=4):
-    runs = []
-
-    prev_x = lx[0]
-    prev_y = ly[0]
-    cur_len = 1
-
-    for x, y in zip(lx[1:], ly[1:]):
-        if x - prev_x != 1 or y != prev_y:
-            if cur_len >= min_len:
-                runs.append((x.item() - cur_len, cur_len))
-            cur_len = 1
-        else:
-            cur_len += 1
-
-        prev_x = x
-        prev_y = y
-
-    if cur_len >= min_len:
-        runs.append((prev_x.item() - cur_len, cur_len))
-
-    return runs
-
+"""
