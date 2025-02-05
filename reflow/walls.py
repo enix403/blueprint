59f094ce86d336672984820b069319e9a9784a29
enix403 <fog.code000@gmail.com>
2025-02-05 18:28:47 +0500

    delete duplicate old code

--- reflow/walls.py
+++ reflow/walls.py
@@ -72,7 +72,6 @@ def scale_sep_mask(mask: torch.Tensor, scale_x: int, scale_y: int) -> torch.Tens
 
     return scaled_mask
 
-
 # -----------------------------------
 
 def extract_face_walls(sep_mask):
@@ -107,79 +106,3 @@ def extract_face_walls(sep_mask):
     )
 
     return [up_walls, right_walls, down_walls, left_walls]
-
-
-"""
-
-_dirs = [
-    (-1, -1, CC_TL),
-    (-1,  0, CC_T ),
-    (-1,  1, CC_TR ),
-
-    ( 0, -1, CC_L ),
-    ( 0,  1, CC_R ),
-
-    ( 1, -1, CC_BL),
-    ( 1,  0, CC_B ),
-    ( 1,  1, CC_BR),
-]
-
-
-def create_sep_mask_slow(room_mask: torch.tensor):
-    len_x, len_y = room_mask.shape
-
-    sep_mask = torch.zeros(room_mask.shape, dtype=torch.uint8)
-
-    for x in range(len_x):
-        for y in range(len_y):
-            cell_room = room_mask[x, y]
-
-            for dx, dy, bit in _dirs:
-                nx = x + dx
-                ny = y + dy
-
-                if nx < 0 or nx >= len_x or ny < 0 or ny >= len_y:
-                    continue
-
-                n_room = room_mask[nx, ny]
-
-                if cell_room > n_room:
-                    sep_mask[x, y] += bit
-
-    return sep_mask
-
-
-def scale_sep_mask_slow(
-    sep_mask: torch.tensor,
-    scale_x: int, scale_y: int
-):
-    len_x, len_y = sep_mask.shape
-
-    scaled_sep_mask = torch.zeros(
-        (len_x * scale_x, len_y * scale_y),
-        dtype=torch.uint8
-    )
-
-    for x in range(len_x):
-        for y in range(len_y):
-            bx = x * scale_x
-            by = y * scale_y
-
-            nx = bx + scale_x
-            ny = by + scale_y
-
-            view = scaled_sep_mask[bx:nx, by:ny]
-            src = sep_mask[x, y].item()
-
-            view[ 0,  0] += src & CC_TL
-            view[ 0, -1] += src & CC_TR
-            view[-1, -1] += src & CC_BR
-            view[-1,  0] += src & CC_BL
-
-            view[ 0,  :] += src & CC_T
-            view[ :, -1] += src & CC_R
-            view[-1,  :] += src & CC_B
-            view[ :,  0] += src & CC_L
-
-    return scaled_sep_mask
-"""
